7ddbeb63e134d7bb215d302c5b6c665a9915ee6a
Lucas Kuhlmann <lucas.khmn@gmail.com>
2025-04-10 19:34:42 -0300

    Creating entity Post with user nested

---
+++ src/main/java/com/lucaskuhlmann/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.lucaskuhlmann.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucaskuhlmann.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
